47f10f0903183207d248e544f3c323ba14fe5e62
JNS <65025344+jinto4638@users.noreply.github.com>
2020-06-30 14:58:16 +0530

    Update set_watermark_text.py

--- bot/plugins/set_watermark_text.py
+++ bot/plugins/set_watermark_text.py
@@ -16,14 +16,14 @@ async def _(c, m):
     
     if len(m.command) == 1:
         await m.reply_text(
-            text="You can add custom watermark text to the screenshots.\n\nUsage: `/set_watermark text`. Text should not Exceed 30 characters.",
+            text="You can add custom watermark text to the screenshots.\n\nUsage: `/set_watermark text`. Text should not Exceed 40 characters.",
             quote=True,
             parse_mode="markdown"
         )
         return
     
     watermark_text = ' '.join(m.command[1:])
-    if len(watermark_text) > 30:
+    if len(watermark_text) > 40:
         await m.reply_text(
             text=f"The watermark text you provided (__{watermark_text}__) is `{len(watermark_text)}` characters long! You cannot set watermark text greater than 30 characters.",
             quote=True,
